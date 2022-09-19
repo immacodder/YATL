b48645f690b3165fd3a9ee8c844e5e63ba3fe1fd
Tigran Khachaturian <yande.myr@tutanota.com>
2022-09-19 13:02:50 +0200

    popup now renders on top of anchor if there isn't enough space below

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -16,40 +16,57 @@ interface P {
 export default function Popup(p: P) {
 	const [position, setPosition] = useState({
 		offsetRight: null as number | null,
-		bottom: null as number | null,
+		distanceFromTop: null as number | null,
 	})
-	const wrapperNodeRef = useRef<HTMLDivElement>(null)
+	const wrapperRef = useRef<HTMLDivElement>(null)
 	const { width: windowWidth } = useWindowResize()
 
 	useEffect(() => {
-		if (p.anchor) {
-			const { right, bottom } = getCoords(p.anchor)
-			let offsetFromRight = innerWidth - right
-			const wrapperLeft =
-				wrapperNodeRef.current?.getBoundingClientRect().left ?? 0
+		if (p.anchor && wrapperRef.current) {
+			const {
+				right: anchorRight,
+				bottom: anchorBottom,
+				top: anchorTop,
+			} = getCoords(p.anchor)
+			const { bottom: wrapperBottom, height: wrapperHeight } =
+				wrapperRef.current.getBoundingClientRect()
+
+			let adjustedBottom = anchorBottom
+			if (innerHeight - wrapperBottom < 0) {
+				// means that wrapper is outside the screen
+				adjustedBottom = anchorTop - wrapperHeight
+			}
+
+			let offsetFromRight = innerWidth - anchorRight
+			const wrapperLeft = wrapperRef.current?.getBoundingClientRect().left ?? 0
 			if (wrapperLeft < 0) {
 				offsetFromRight = offsetFromRight - (Math.abs(wrapperLeft) + 16)
 			}
 
-			setPosition({ offsetRight: offsetFromRight, bottom })
+			setPosition({
+				offsetRight: offsetFromRight,
+				distanceFromTop: adjustedBottom,
+			})
 		}
-	}, [p, windowWidth, wrapperNodeRef])
+		// If I don't disable it, it will not allow me to use wrapperRef.current, which I need to use in this instance
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [p, windowWidth, wrapperRef, wrapperRef.current])
 
 	function getCoords(elem: HTMLElement) {
 		const box = elem.getBoundingClientRect()
 
 		return {
-			top: box.top + window.pageYOffset,
-			right: box.right + window.pageXOffset,
-			bottom: box.bottom + window.pageYOffset,
-			left: box.left + window.pageXOffset,
+			top: box.top + pageYOffset,
+			right: box.right + pageXOffset,
+			bottom: box.bottom + pageYOffset,
+			left: box.left + pageXOffset,
 		}
 	}
 
 	let wrapperStyles: CSSProperties
 	if (p.type === "anchor")
 		wrapperStyles = {
-			top: (position.bottom ?? 0) + "px",
+			top: (position.distanceFromTop ?? 0) + "px",
 			right: `${position.offsetRight}px`,
 			...p.wrapperStyles,
 		}
@@ -59,7 +76,7 @@ export default function Popup(p: P) {
 			top: `${
 				innerHeight -
 				innerHeight / 2 -
-				(wrapperNodeRef?.current?.getBoundingClientRect().height ?? 120) / 2
+				(wrapperRef?.current?.getBoundingClientRect().height ?? 120) / 2
 			}px`,
 			...p.wrapperStyles,
 		}
@@ -76,7 +93,7 @@ export default function Popup(p: P) {
 			style={{ height: `${getScrollHeight()}px` }}
 		>
 			<div
-				ref={wrapperNodeRef}
+				ref={wrapperRef}
 				className={
 					p.wrapperClassNames
 						? `absolute rounded bg-white ${p.wrapperClassNames}`
