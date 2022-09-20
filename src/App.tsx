122c9925911d21a8d23afe0aa2a531634776567d
Tigran Khachaturian <yande.myr@tutanota.com>
2022-09-20 13:12:53 +0200

    styling of popup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { AppRouter } from "./components/AppRouter"
 import { useDataFetch } from "./hooks/useDataFetch"
-import "reactjs-popup/dist/index.css"
 
 export function App() {
 	const { todos } = useDataFetch()
